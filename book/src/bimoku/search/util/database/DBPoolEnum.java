d2f8a02e2d563bc3d0e51148350fe6aa706b1b38
sun <sun@sun-PC>
2013-08-23 17:46:37 +0800

    添加DBPoolEnum类，实现多个数据库操作

---
+++ book/src/bimoku/search/util/database/DBPoolEnum.java
@@ -0,0 +1,15 @@
+package bimoku.search.util.database;
+
+/**
+ * 枚举数据库链接池的个数
+ * DB1 ip:192.168.0.1 
+ * DB2 ip:localhost
+ * DB3 ip:...
+ * @author sun
+ *
+ */
+public enum DBPoolEnum {
+	
+	DB1,DB2,DB3;
+
+}
